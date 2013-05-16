471b537c3cf7f27db26704d3c9efe697c04b6d63
Ahmad Ariful Amri <ooh.begitu@gmail.com>
2013-05-16 08:42:47 +0700

    Create index.php
    .php

---
+++ index.php
@@ -0,0 +1,51 @@
+  	<head>
+			<title>Selamat datang</title>
+			<LINK rel="stylesheet" TYPE="text/css" href="css.css">
+			<!--link href='http://fonts.googleapis.com/css?family=Open+Sans:700italic,400' rel='stylesheet' type='text/css'/-->
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		</head>
+		<div id='kepala'>
+			<div id='salam' align='center'><b>Pusat Pelayanan Data Rekam Medik RS HMIF</b></div>
+		</div>
+		
+		<div id='basabasi'>
+			<div class='basa-basi'><b>
+				disini untuk basa basi menjelaskan deskripsi dari fasilitas rekam medik yang ada disebuah rumah sakit
+				sehingga nampak keren, tidak hambar dan kosong.. </b>
+			</div>
+		</div>
+
+	<body>		
+		<div id='badan'>
+			<div id='wrapper'>
+				<div id='mainbar'>
+					<div id='dua'> 
+						<div class='kiri'> 
+							<a href='logindokter.php'><img src='/rekammedik/gambar/dokter.png'/></a>
+							<center>
+                            <a href='logindokter.php'> <p class='tulisan'>Dokter</p></a>
+							</center>
+						</div> 
+						<div class='kanan'> 
+							<a href='loginstaff.php'><img src='/rekammedik/gambar/staf.png'/></a>
+							<center>
+                            <a href='loginstaff.php'><p class='tulisan'>Staff</p></a>
+							</center>
+						</div>
+					</div> 
+					<div id='satu'> 
+						<img src='/rekammedik/gambar/pasien.png'/></a>
+						<center>
+                        <a href='loginpasien.php'><p class='tulisan'>Pasien</a>
+						</center>
+					</div>
+				</div> 
+			</div>
+		</div>
+		
+		<div id='kaki'>
+			<div class='kauskaki'>
+				<p>by Kelompok 1: Ahmad A.A, R.Phonna, Rabiatul, M.Henrizal, Iin Athailah, Mahdar</p>
+			</div>
+		</div>
+	</body>
